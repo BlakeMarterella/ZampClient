7d72c29a68b0941c02db482e612e1f1e5446a849
James Biser <Jbiser361@yahoo.com>
2021-04-10 01:37:16 -0400

    Made the survey page and an email feild that has a basic text feild

--- assets/js/survey.js
+++ assets/js/survey.js
@@ -1,4 +1,4 @@
-//Load Config stuff for Firebase
+//Load Config stuff for Firebase, main part of the survey
 var firebaseConfig = {
     apiKey: "AIzaSyCl7crXlHPqniJeASl4T1KJEuItdRpv64E",
     authDomain: "projecto-ae74c.firebaseapp.com",
@@ -13,8 +13,10 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 //firebase.analytics(); //Maybe add that? idk 
 //Makes a ref to the company survey, concatanates the company name + survey to find easily. 
-var comapanyName = "CompanyA" //created a var to manipuate from PHP or somshit like that
-var messagesRef = firebase.database().ref(comapanyName + 'survey');
+var compName = localStorage.getItem("compSurvey"); 
+var comapanyName = compName
+
+
 //creates an event listen for when the submit button is pressed, and calls the funtion submitForm
 
 document.getElementById('post').addEventListener('submit', submitForm); //<-- the submitForm is called when the listener is activated
@@ -48,6 +50,8 @@ function getInputVal(id) {
 
 //save survey elemets from form
 function saveSurvey(firstName, lastName, company, streetAddress,swag) {
+    console.log(comapanyName)
+    var messagesRef = firebase.database().ref(comapanyName + 'survey');
     var newMessageRef = messagesRef.push(); //pushes the data from the parameters into firebase
     newMessageRef.set({
         firstName: firstName,
@@ -57,3 +61,25 @@ function saveSurvey(firstName, lastName, company, streetAddress,swag) {
         swagType : swag
     });
 }
+
+function loadProfile(items) {
+    
+    items.forEach( item => {
+      document.getElementById('description').innerHTML = item.tempText;
+      document.getElementById('epic').innerText = "you got some swag by " + item.alias + "!"
+
+    });
+}
+
+var databaseRefPro = firebase.database().ref(comapanyName);
+
+databaseRefPro.once('value',function(snapshot){
+    snapshot.forEach(function(childsnapshot) { 
+        var childKey = childsnapshot.key;
+        var childData = childsnapshot.val();
+        const items1 = [
+            { id: childKey, alias: childData.alias, color: childData.color , imageurl: childData.imageUrl, tempText : childData.templateText, compName: childData.companyName}
+        ];
+        loadProfile(items1);
+    })
+})
---
+++ assets/js/surveyLogin.js
@@ -0,0 +1,46 @@
+//where the email gets filtered
+
+function checker(items){
+  items.forEach( item => {
+
+    var ret = document.getElementById("email_field").value.replace('.','');
+    console.log(ret);
+    if ( ret == item.id) {
+        console.log(item.comp);
+        localStorage.setItem("compSurvey", item.comp); 
+        console.log( localStorage.getItem("compSurvey"));
+     window.location.href = "survey.html";
+     console.log(item.comp);
+    }
+  })
+}
+function nice() {
+var databaseRefPro = firebase.database().ref("Employees");
+
+
+    databaseRefPro.once('value',function(snapshot){
+       snapshot.forEach(function(childsnapshot) { 
+            var childKey = childsnapshot.key;
+         var childData = childsnapshot.val();
+            const items1 = [
+               { id: childKey, comp : childData.company }
+           ];
+         checker(items1);
+     })
+    })
+}
+
+
+function login(){
+    nice()
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
+
+document.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    login()
+  }
+})
