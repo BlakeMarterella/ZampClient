090df99f2be263218af8c27bdf8077988a067d85
blakemarterella <blake@marterella.com>
2021-04-15 11:30:09 -0400

    begin adding interactive cards

--- assets/js/swag.js
+++ assets/js/swag.js
@@ -14,3 +14,14 @@ firebase.auth().onAuthStateChanged(function(user) {
   });
   
   document.getElementById('compName').innerText = localStorage.getItem("alias");
+
+var arr = [
+    { "Name": "Peter", "Job": "Programmer" },
+    { "Name": "John", "Job": "Programmer" },
+    { "Name": "Kevin", "Job": "Scientist" },
+];
+
+$.each(arr, function (i) {
+    var templateString = '<div class="col-md-4"><div class="card mb-2 box-shadow"><img class="card-img-top" src="http://safarset.com/wp-content/uploads/2019/05/IMG-20190423-WA0029.jpeg" alt="Card image cap"><div class="card-body"><h5>' + arr[i].Name + '</h5></p></div></div></div>';
+    $('#cards').append(templateString);
+})
