e6b6173cb9863d83b5a5a5c337145b336842403f
James Biser <Jbiser361@yahoo.com>
2021-04-20 18:42:43 -0400

    fixed bug where the popup would disapear when the add button is pressed.

--- public/assets/js/companyProfile.js
+++ public/assets/js/companyProfile.js
@@ -1,18 +1,18 @@
-type = ['','info','success','warning','danger'];
+type = ['', 'info', 'success', 'warning', 'danger'];
 
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      // User is signed in.
-  
-      if(user != null){
-             
-      }
-  
+        // User is signed in.
+
+        if (user != null) {
+
+        }
+
     } else {
-      // No user is signed in.
-      window.location.href = "index.html";
+        // No user is signed in.
+        window.location.href = "index.html";
     }
-  });
+});
 
 var color = ""
 var compName = localStorage.getItem("comp");
@@ -20,81 +20,126 @@ var name = compName
 
 function loadProfile(items) {
     console.log(name);
-    
-    items.forEach( item => {
-    document.getElementById('compName').innerText = item.alias
-       document.getElementById('template').value = item.tempText;
-       document.getElementById('comp').placeholder = item.compName;
-       document.getElementById('alias').value = item.alias;
-       document.getElementById('title').innerHTML = item.alias;
-       document.getElementById('image2').src = item.imageurl
-       document.getElementById('small').innerHTML = item.compName;
+
+    items.forEach(item => {
+        document.getElementById('compName').innerText = item.alias
+        document.getElementById('template').value = item.tempText;
+        document.getElementById('comp').placeholder = item.compName;
+        document.getElementById('alias').value = item.alias;
+        document.getElementById('title').innerHTML = item.alias;
+        document.getElementById('image2').src = item.imageurl
+        document.getElementById('small').innerHTML = item.compName;
     });
 }
 
 function loadTableData(items) {
     const table = document.getElementById("employees");
-    items.forEach( item => {
-      let row = table.insertRow();
-      let id = row.insertCell(0);
-      let name = row.insertCell(1);
-      let lastname = row.insertCell(2);
-      let email = row.insertCell(3);
-      id.innerHTML = item.id;
-      name.innerHTML = item.firstName;
-      lastname.innerHTML = item.lastName;
-      email.innerHTML = item.email;
-      color = item.color;
+    items.forEach(item => {
+        let row = table.insertRow();
+        let id = row.insertCell(0);
+        let name = row.insertCell(1);
+        let lastname = row.insertCell(2);
+        let email = row.insertCell(3);
+        id.innerHTML = item.id;
+        name.innerHTML = item.firstName;
+        lastname.innerHTML = item.lastName;
+        email.innerHTML = item.email;
+        color = item.color;
     });
 }
 
-var databaseRef = firebase.database().ref(name+'Admin');
+var databaseRef = firebase.database().ref(name + 'Admin');
 var databaseRefPro = firebase.database().ref(name);
 
 var rowIndex = 1;
 
-databaseRefPro.once('value',function(snapshot){
-    snapshot.forEach(function(childsnapshot) { 
+databaseRefPro.once('value', function (snapshot) {
+    snapshot.forEach(function (childsnapshot) {
         var childKey = childsnapshot.key;
         var childData = childsnapshot.val();
-        const items1 = [
-            { id: childKey, alias: childData.alias, color: childData.color , imageurl: childData.imageUrl, tempText : childData.templateText, compName: childData.companyName}
-        ];
+        const items1 = [{
+            id: childKey,
+            alias: childData.alias,
+            color: childData.color,
+            imageurl: childData.imageUrl,
+            tempText: childData.templateText,
+            compName: childData.companyName
+        }];
         loadProfile(items1);
     })
 })
 
-databaseRef.once('value',function(snapshot){
-    snapshot.forEach(function(childsnapshot) { 
+databaseRef.once('value', function (snapshot) {
+    snapshot.forEach(function (childsnapshot) {
         var childKey = childsnapshot.key;
         var childData = childsnapshot.val();
-        const items1 = [
-            { id: childKey,firstName: childData.firstName, lastName: childData.lastName , email: childData.email}
-        ];
+        const items1 = [{
+            id: childKey,
+            firstName: childData.firstName,
+            lastName: childData.lastName,
+            email: childData.email
+        }];
         loadTableData(items1);
     })
 })
-  
-function updateProfile(){
+
+function addEmployee() {
+    let firstName = document.getElementById('firstname').value
+    let lastName = document.getElementById('lastname').value
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
+
+    if (firstName != "" && lastName != "" && email != "" && password != "") {
+        var userA = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email
+        };
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in
+                firebase.database().ref(this.name + "Admin").push(userA);
+                var ret = email.replace('.', '');
+                var em = {
+                    company: name,
+                };
+                firebase.database().ref("ids").child(ret).set(em);
+                location.reload()
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode + ' - ' + errorMessage);
+            });
+
+    } else {
+        window.alert("Please fill out the required data.")
+    }
+}
+
+
+function updateProfile() {
     var root = firebase.database().ref();
 
-    let template =  document.getElementById('template').value;
-    let name = document.getElementById('comp').placeholder; 
-     let alias =  document.getElementById('alias').value;
+    let template = document.getElementById('template').value;
+    let name = document.getElementById('comp').placeholder;
+    let alias = document.getElementById('alias').value;
     let url = document.getElementById('image').value;
     console.log(url)
-       var root = firebase.database().ref();
+    var root = firebase.database().ref();
     // document.getElementById('title').innerHTML; 
     // document.getElementById('image').src; 
     // document.getElementById('small').innerHTML;
 
     var user = {
-        alias : alias,
-        templateText : template,
+        alias: alias,
+        templateText: template,
         companyName: name,
-        imageUrl : url,
+        imageUrl: url,
     };
-        root.child(this.name).child("randomID0").update(user);
+    root.child(this.name).child("randomID0").update(user);
+
     location.reload()
 }
 
