0529dd2ef2f7c6722c38685148689e6da6f3388f
blakemarterella <blake@marterella.com>
2021-06-15 15:03:48 -0400

    readd cards

--- public/admin/assets/js/swag.js
+++ public/admin/assets/js/swag.js
@@ -26,23 +26,6 @@ firebase.auth().onAuthStateChanged(function (user) {
     }
 });
 
-
-//   var templateString =
-//  '<div class="col-md-4">' +
-//  '<div class="card mb-2 box-shadow">' +
-//  '<img class="card-img" src=' + 'image-url-here'+ ' alt="Card image cap">' +
-//  '<div class="content">' +
-//  '<h5 class="card-title">' + 'This is my name' + '</h5> ' +
-//  '<p class="card-description">' + 'This is my id' + '</p>' +
-//  '<p class="card-description">' + 'This is how much stock I have left' + '</p>' +
-//  '<br>' +
-//  '<button class="btn btn-primary card-button">Edit</button>' +
-//  '<button class="btn btn-fill btn-danger card-button pull-right">Delete</button>' +
-//  '</div>' +
-//  '</div>' +
-//  '</div>';
-//  $("#cards").append(templateString); 
-
 var databaseRefPro = firebase.database().ref("Swag");
 var arr = []
 
@@ -83,7 +66,6 @@ function loadItems(item) {
         '</div>';
 
        ('#cards').append(templateString);
-
       const dic = {
         index: counter,
         id: item[i].id,
