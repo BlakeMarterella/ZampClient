8037be669b671e3107fd9fc801b8dee34fe18ca5
blakemarterella <blake@marterella.com>
2021-06-16 00:39:48 -0400

    add interactive links

--- public/admin/assets/js/clients.js
+++ public/admin/assets/js/clients.js
@@ -11,10 +11,9 @@ var templateString =
   '<div class="card mb-2 box-shadow">' +
     '<img class="card-img" src=' + 'https://ihfiles.com/products/236/26859/p/4/522808.jpeg' + ' alt="company picture here">' +
     '<div class="content">' +
-      '<h5 class="card-title">' + 'Company Name Here' + '</h5> ' +
+      '<h5 class="card-title"><a href="client-info.html">' + 'Company Name Here' + '</a></h5> ' +
       '<br>' +
-      '<button class="btn btn-primary card-button">More Info</button>' +
-      '<button class="btn btn-fill btn-danger card-button pull-right">Delete</button>' +
+      '<button class="btn btn-fill btn-danger card-button">Delete</button>' +
     '</div>' +
   '</div>' +
 '</div>';
