f8d29b9375ae198d52ec0e1ec4070985246e3afa
blakemarterella <blake@marterella.com>
2021-06-15 23:52:37 -0400

    finish order center table UX

---
+++ public/admin/assets/js/orders.js
@@ -0,0 +1,78 @@
+
+var table = new Tabulator("#orders-table", {
+    layout: "fitColumns", //fit columns to width of table
+    responsiveLayout: "hide", //hide columns that dont fit on the table
+    tooltips: true, //show tool tips on cells
+    addRowPos: "top", //when adding a new row, add it to the top of the table
+    history: true, //allow undo and redo actions on the table
+    pagination: "local", //paginate the data
+    paginationSize: 30, //allow 30 rows per page of data
+    movableColumns: true, //allow column order to be changed
+    resizableRows: true, //allow row order to be changed
+    initialSort: [ //set the initial sort order of the data
+        {
+            column: "lastname",
+            dir: "asc"
+        },
+    ],
+    columns: [ //define the table columns
+        {
+            formatter: "rowSelection",
+            titleFormatter: "rowSelection",
+            width: 20,
+            hozAlign: "center",
+            headerSort: false,
+            cellClick: function (e, cell) {
+                cell.getRow().toggleSelect();
+            }
+        },
+        {
+            title: "First Name",
+            field: "firstname",
+            editor: "input"
+        },
+        {
+            title: "Last Name",
+            field: "lastname",
+            editor: "input",
+
+            
+        },
+        {
+            title: "Priority",
+            field: "priority",
+            editor: "select",
+            width: 100,
+
+            editorParams: {
+                values: ["low", "medium", "high"]
+            }
+        },
+        {
+            title: "Email",
+            field: "email",
+            editor: "input"
+        },
+        {
+            title: "Address",
+            field: "address",
+            editor: "input"
+        },
+        {
+            title: "Date Entered",
+            field: "date",
+            editor: "none"
+        },
+        {
+            title: "Shipping Number",
+            field: "shipping",
+            editor: "input"
+        },
+        {
+            title: "ID",
+            field: "ID",
+            width: 200,
+            editor: "none"
+        },
+    ],
+});
