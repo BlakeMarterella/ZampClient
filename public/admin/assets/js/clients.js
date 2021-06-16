f4a3894bae02f2caffa506a30f1a96aa0739f235
blakemarterella <blake@marterella.com>
2021-06-16 00:14:31 -0400

    finish cards for client table

--- public/admin/assets/js/clients.js
+++ public/admin/assets/js/clients.js
@@ -8,16 +8,15 @@ function openForm() {
 
 var templateString =
 '<div class="col-md-4">' +
-'<div class="card mb-2 box-shadow">' +
-'<img class="card-img" src=' + 'image-url-here'+ ' alt="Card image cap">' +
-'<div class="content">' +
-'<h5 class="card-title">' + 'This is my name' + '</h5> ' +
-'<p class="card-description">' + 'This is my id' + '</p>' +
-'</br>' +
-'<button class="btn btn-primary card-button pull-left">Edit</button>' +
-'<button class="btn btn-fill btn-danger card-button pull-right">Delete</button>' +
-'</div>' +
-'</div>' +
+  '<div class="card mb-2 box-shadow">' +
+    '<img class="card-img" src=' + 'https://ihfiles.com/products/236/26859/p/4/522808.jpeg' + ' alt="company picture here">' +
+    '<div class="content">' +
+      '<h5 class="card-title">' + 'Company Name Here' + '</h5> ' +
+      '<br>' +
+      '<button class="btn btn-primary card-button">Edit</button>' +
+      '<button class="btn btn-fill btn-danger card-button pull-right">Delete</button>' +
+    '</div>' +
+  '</div>' +
 '</div>';
 
 $("#clients").append(templateString);
