751e82ee58b795f7f646f45b1d55765b0a0b0d16
blakemarterella <blake@marterella.com>
2021-06-17 11:32:12 -0400

    detect time and provide custom welcome message based on the time

--- public/admin/assets/js/dashboard.js
+++ public/admin/assets/js/dashboard.js
@@ -11,3 +11,15 @@ firebase.auth().onAuthStateChanged(function (user) {
       window.location.href = "index.html";
     }
   });
+
+var welcome = document.getElementById('welcome-msg');
+welcome.innerHTML = "Welcome";  
+var curTime = new Date().getHours();
+if (curTime > 0 && curTime < 12) {
+   welcome.innerHTML = "Good Morning";  
+} else if (curTime < 16) {
+  welcome.innerHTML = "Good Afternoon";  
+} else {
+  welcome.innerHTML = "Good Evening";  
+}
+welcome.append(", Jill!");
