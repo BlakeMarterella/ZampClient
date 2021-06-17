fb52a06ac795465bad8f51e14b198933d9e09eab
blakemarterella <blake@marterella.com>
2021-06-16 20:20:18 -0400

    change the add client view

--- public/admin/assets/js/swag.js
+++ public/admin/assets/js/swag.js
@@ -54,7 +54,7 @@ function loadItems(item) {
     '</div>' +
     '</div>' +
     '</div>';
-    $("#cards").append(templateString);
+    $("#swag").append(templateString);
 
     const dic = {
       index: counter,
@@ -87,21 +87,3 @@ function getID(num) {
 
 
 }
-
-
-
-// var templateString =
-// '<div class="col-md-4">' +
-// '<div class="card mb-2 box-shadow">' +
-// '<img class="card-img" src=' + 'image-url-here'+ ' alt="Card image cap">' +
-// '<div class="content">' +
-// '<h5 class="card-title">' + 'This is my name' + '</h5> ' +
-// '<p class="card-description">' + 'This is my id' + '</p>' +
-// '<p class="card-description">' + 'This is how much stock I have left' + '</p>' +
-// '<br>' +
-// '<button class="btn btn-primary card-button">Edit</button>' +
-// '<button class="btn btn-fill btn-danger card-button pull-right">Delete</button>' +
-// '</div>' +
-// '</div>' +
-// '</div>';
-// $("#cards").append(templateString);
