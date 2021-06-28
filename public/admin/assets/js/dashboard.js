92420626df44f079522c07529880b3b935195abb
blakemarterella <blake@marterella.com>
2021-06-28 00:24:52 -0400

    fuck logic again

--- public/admin/assets/js/dashboard.js
+++ public/admin/assets/js/dashboard.js
@@ -28,7 +28,7 @@ welcome.innerHTML = "Welcome";
 var curTime = new Date().getHours();
 if (curTime > 0 && curTime < 12) {
    welcome.innerHTML = "Good Morning";  
-} else if (curTime < 16) {
+} else if (curTime >= 12 && curTime < 16) {
   welcome.innerHTML = "Good Afternoon";  
 } else {
   welcome.innerHTML = "Good Evening";  
